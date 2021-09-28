67368cac518821c76e56d148c7f5f5abb8dce710
Alex Coats <swifteralex@users.noreply.github.com>
2021-09-28 13:38:46 -0700

    Program functions as intended. Will look into using GPU for faster performance next

--- mainwin.cpp
+++ mainwin.cpp
@@ -2,7 +2,7 @@
 #include <d2d1.h>
 #pragma comment(lib, "d2d1")
 #include "basewin.h"
-#include <vector>
+#include <complex>
 
 class MainWindow : public BaseWindow<MainWindow> {
 private:
@@ -10,9 +10,14 @@ private:
 	ID2D1HwndRenderTarget* pRenderTarget = 0;
 	ID2D1SolidColorBrush* pBrush = 0;
 	HCURSOR hCursor = LoadCursor(NULL, IDC_ARROW);
+	std::complex<double> top_left = std::complex<double>(-2, 1.3);
+	double zoom = -2.15;
+	D2D1_POINT_2F last_pos = D2D1::Point2F(0, 0);
+	bool panning = false;
 
 public:
 	void DrawScene();
+	std::complex<double> CanvasCoordToActual(UINT X, UINT Y);
 	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 	PCWSTR ClassName() const { return L"Basic Window Class"; }
 };
@@ -27,10 +32,21 @@ void MainWindow::DrawScene() {
 	UINT8* Data = (UINT8*)malloc((LONGLONG)size.height * size.width * 4);
 	for (UINT Y = 0; Y < size.height; Y++) {
 		for (UINT X = 0; X < size.width; X++) {
+			std::complex<double> z = std::complex<double>(0, 0);
+			std::complex<double> c = CanvasCoordToActual(X, Y);
+			bool not_in_set = false;
+			for (int i = 0; i < 100; i++) {
+				if (abs(z) >= 2) {
+					not_in_set = true;
+					break;
+				}
+				z = z * z + c;
+			}
+
 			UINT8* PixelData = Data + ((Y * (LONGLONG)size.width) + X) * 4;
-			PixelData[0] = 0;
-			PixelData[1] = (X + Y) % 255;
-			PixelData[2] = (int)(X * 0.05 + Y * 0.05) % 255;
+			PixelData[0] = not_in_set * 255;
+			PixelData[1] = not_in_set * 255;
+			PixelData[2] = not_in_set * 255;
 			PixelData[3] = 255;
 		}
 	}
@@ -48,8 +64,55 @@ void MainWindow::DrawScene() {
 	pRenderTarget->EndDraw();
 }
 
+std::complex<double> MainWindow::CanvasCoordToActual(UINT X, UINT Y) {
+	std::complex<double> res = std::complex<double>((double)X * pow(10, zoom), -((double)Y * pow(10, zoom)));
+	res += top_left;
+	return res;
+}
+
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
+		case WM_LBUTTONDOWN: {
+			panning = true;
+			return 0;
+		}
+
+		case WM_LBUTTONUP: {
+			panning = false;
+			return 0;
+		}
+
+		case WM_MOUSEWHEEL: {
+			GET_WHEEL_DELTA_WPARAM(wParam);
+			if ((int)wParam > 0) {
+				double real = (last_pos.x) * pow(10, zoom) / 4.9;
+				double imag = -(last_pos.y) * pow(10, zoom) / 4.9;
+				top_left += std::complex<double>(real, imag);
+				zoom -= 0.1;
+			} else {
+				double real = (last_pos.x) * pow(10, zoom) / 3.9;
+				double imag = -(last_pos.y) * pow(10, zoom) / 3.9;
+				top_left -= std::complex<double>(real, imag);
+				zoom += 0.1;
+			}
+			DrawScene();
+			return 0;
+		}
+
+		case WM_MOUSEMOVE: {
+			float x = LOWORD(lParam);
+			float y = HIWORD(lParam);
+			if (panning) {
+				float x_diff = (last_pos.x - x) * pow(10, zoom);
+				float y_diff = (y - last_pos.y) * pow(10, zoom);
+				std::complex<double> c = std::complex<double>((double)x_diff, (double)y_diff);
+				top_left += c;
+				DrawScene();
+			}
+			last_pos = D2D1::Point2F(x, y);
+			return 0;
+		}
+
 		case WM_SIZE: {
 			if (wParam != SIZE_MINIMIZED) {
 				DrawScene();
